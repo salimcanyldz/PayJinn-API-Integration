38e325586374b1c375d23923995522ebf1a41060
Salim Can <slmcnyldz1@gmail.com>
2021-07-01 21:32:04 +0300

    cors added

--- src/main/java/com/payjinn/app/PayjinnTaskController.java
+++ src/main/java/com/payjinn/app/PayjinnTaskController.java
@@ -3,6 +3,7 @@ package com.payjinn.app;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ public class PayjinnTaskController {
 	@Value("${auth.password}")
 	private String password;
 
+	@CrossOrigin
     @GetMapping("/")
     public ResponseEntity<String> startOperation(){
     	try {
